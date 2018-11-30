a3ca5e5e9e45d9ea4ba68cf9f311184df579f188
Brett777 <brett@datascience.com>
2018-11-29 22:48:20 -0500

    updates

--- guru.py
+++ guru.py
@@ -1,6 +1,8 @@
+import json
 import pandas as pd
 from pandas.io.json import json_normalize
 import requests
+
 
 
 key = '6b57487789d6f79fe484f7a043be8f90:51fcb919d122fdaa2e7bb1cf9d2d680b'
@@ -12,16 +14,9 @@ base_url = "https://api.gurufocus.com/public/user/"
 
 response = requests.get(base_url + key + "/stock/" + stock + "/financials")
 data = response.json()
-df = pd.read_json(data, typ='series', orient='columns')
+df = pd.DataFrame.from_dict(data, orient="index")
 
 
 
-
-
-df = pd.io.json.json_normalize(data, '')
-#df.columns = df.columns.map(lambda x: x.split(".")[-1])
-
-print(df.shape)
-
-
-print(df)
+df2 = json_normalize(data).apply(pd.Series).stack().reset_index()
+df2[0] = df2[0].apply(pd.Series, index=df2.iloc[0,2])
